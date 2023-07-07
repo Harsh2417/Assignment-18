bdbcb6be91a3eebe88511b1ea5982d96c3bb150e
Harsh2417 <65455754+Harsh2417@users.noreply.github.com>
2023-07-07 11:28:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import {useEffect, useRef, useState} from 'react';
+import './App.css';
+
+function App() {
+  const [time,setTime]= useState(0);
+  useEffect(()=>{
+    
+    
+  return()=> clearInterval(id.current);
+
+  }, []);
+  let id= useRef();
+  function handleTime(){
+   id.current= setInterval(()=>{
+    setTime((prev)=>prev+1);
+    console.log(time);
+  }, 1000);
+
+  }
+  
+  return (
+    <div className="App">
+      <h1>Stopwatch {time}</h1>
+      <button onClick={() => handleTime()}>start</button>
+      <button onClick={() => clearInterval(id.current)}>pause</button>
+      <button onClick={() =>{
+         clearInterval(id.current)
+         setTime(0);
+         }}>reset</button>
+    </div>
+  );
+}
+
+export default App;
